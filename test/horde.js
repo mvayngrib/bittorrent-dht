c61012486f84536c05734a6bde9a7e5783783981
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-09-27 13:16:06 +0100

    fix horde test

--- test/horde.js
+++ test/horde.js
@@ -3,36 +3,40 @@ var parallel = require('run-parallel')
 var common = require('./common')
 var DHT = require('../')
 
-test('announce+lookup with 2-20 DHTs', function (t) {
+runAnnounceLookupTests()
+
+function runAnnounceLookupTests () {
   var from = 2
   var to = 20
 
-  var numRunning = to - from + 1
   for (var i = from; i <= to; i++) {
     runAnnounceLookupTest(i)
   }
 
   function runAnnounceLookupTest (numInstances) {
-    findPeers(numInstances, t, function (err, dhts) {
-      if (err) throw err
+    var numRunning = numInstances
+    test('announce+lookup with ' + numInstances + ' DHTs', function (t) {
+      findPeers(numInstances, t, function (err, dhts) {
+        if (err) throw err
 
-      dhts.forEach(function (dht) {
-        for (var infoHash in dht.tables) {
-          var table = dht.tables[infoHash]
-          table.toArray().forEach(function (contact) {
-            t.ok(contact.token, 'contact has token')
-          })
-        }
+        dhts.forEach(function (dht) {
+          for (var infoHash in dht.tables) {
+            var table = dht.tables[infoHash]
+            table.toArray().forEach(function (contact) {
+              t.ok(contact.token, 'contact has token')
+            })
+          }
 
-        process.nextTick(function () {
-          dht.destroy(function () {
-            if (--numRunning === 0) t.end()
+          process.nextTick(function () {
+            dht.destroy(function () {
+              if (--numRunning === 0) t.end()
+            })
           })
         })
       })
     })
   }
-})
+}
 
 /**
  *  Initialize [numInstances] dhts, have one announce an infoHash, and another perform a
